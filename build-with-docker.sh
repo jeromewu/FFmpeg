cd5de02a9525c27d24afd663a71fde41c72e0d38
Jerome Wu <jeromewus@gmail.com>
2020-08-26 10:59:10 +0800

    Update build script

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+FLAGS=""
+# Attach TTY only when available, this is for running in Gihub Actions
+if [ -t 1 ]; then FLAGS="-it"; fi
+
 docker pull gcc:9.1
-docker run -it \
+docker run $FLAGS \
   -v $PWD:/usr/src \
   gcc:9.1 \
-  sh -c 'cd /usr/src && ./configure --disable-x86asm && make -j'
+  sh -c 'cd /usr/src && bash ./build.sh'
