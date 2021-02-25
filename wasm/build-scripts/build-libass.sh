543d545cff8b60d949fd42dff31d49a6a37e48eb
Loe Lobo <36975286+loe-lobo@users.noreply.github.com>
2021-02-25 08:07:50 +0000

    fixed libass static reference

--- wasm/build-scripts/build-libass.sh
+++ wasm/build-scripts/build-libass.sh
@@ -8,7 +8,8 @@ LIBASS_PC_PATH="$BUILD_DIR/lib/pkgconfig"
 CONF_FLAGS=(
   --prefix=$BUILD_DIR                                 # install library in a build directory for FFmpeg to include
   --host=i686-gnu                                     # use i686 linux
-  --disable-static
+  --disable-shared
+  --enable-static
   --disable-asm                                       # disable asm optimization
   --disable-fontconfig
   --disable-require-system-font-provider
@@ -17,3 +18,4 @@ echo "CONF_FLAGS=${CONF_FLAGS[@]}"
 (cd $LIB_PATH && ./autogen.sh && EM_PKG_CONFIG_PATH=$LIBASS_PC_PATH emconfigure ./configure "${CONF_FLAGS[@]}")
 emmake make -C $LIB_PATH clean
 emmake make -C $LIB_PATH install -j
+
