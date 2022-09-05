bcce7f52548e0663cdcd541cf89097f1bcd45688
makihiro <87048812+makihiro3@users.noreply.github.com>
2022-09-05 12:35:28 +0000

    Fix dependancy libarary on mp4-scale/png-to-mp4 variants

--- wasm/build-scripts/build-ffmpeg.png-to-mp4.sh
+++ wasm/build-scripts/build-ffmpeg.png-to-mp4.sh
@@ -24,9 +24,9 @@ fi
 mkdir -p "$DISTDIR"
 FLAGS=(
   -I. -I./fftools -I$BUILD_DIR/include
-  -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Llibpostproc -Llibswscale -Llibswresample -L$BUILD_DIR/lib
+  -Llibavcodec -Llibavfilter -Llibavformat -Llibavutil -Llibswscale -Llibswresample -L$BUILD_DIR/lib
   -Wno-deprecated-declarations -Wno-pointer-sign -Wno-implicit-int-float-conversion -Wno-switch -Wno-parentheses -Qunused-arguments
-  -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lpostproc -lm -lx264 -lz
+  -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lm -lx264 -lz
   fftools/ffmpeg_opt.c fftools/ffmpeg_filter.c fftools/ffmpeg_hw.c fftools/cmdutils.c fftools/ffmpeg.c
   -s USE_SDL=2                                  # use SDL2
   -s INVOKE_RUN=0                               # not to run the main() in the beginning
